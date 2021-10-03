d4ef3e958dfbe038e3ce73ef09cbd288ddf4ed2f
mk2020 <mahnaaz85@gmail.com>
2021-10-03 11:11:30 +0200

    redirceted is deleted and add main.js in static(frontend)

--- music_controller/music_controller/urls.py
+++ music_controller/music_controller/urls.py
@@ -21,5 +21,5 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', RedirectView.as_view(url='/api/room')),
+    path('', include('frontend.urls')),
 ]
