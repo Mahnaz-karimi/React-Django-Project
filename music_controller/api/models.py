33412132706464eb68d8df0b386b7df667fe035d
mk2020 <mahnaaz85@gmail.com>
2021-10-07 01:54:35 +0200

    react compomet, router og pages is added

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -9,14 +9,12 @@ def generate_unique_code(): # make a unique number for every room that use for s
     while True:
         code = ''.join(random.choices(string.ascii_uppercase, k=length)) # finde a code with UPPERCASE string and length 6
         if Room.objects.filter(code=code).count() == 0: # if there is a room with det same code, makes a new code,             
-            break # until it can make unique code
+            break # until it can generate a unique code and breaks while-loop
 
     return code
 
 
-# Create models here, and you can put most of logics on models for make a good models
-
-
+# Create models here, and you can put most of logics on models-page for make a good models
 class Room(models.Model):
     code = models.CharField(max_length=8, default="", unique=True)
     host = models.CharField(max_length=50, unique=True) # every room have only one host
