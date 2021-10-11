baa83ccd931b0d60b88e36c0f59fd6d7636e40a9
mk2020 <mahnaaz85@gmail.com>
2021-10-12 01:49:06 +0200

    webpack.config.js is added and changed  url in frontent

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -37,7 +37,7 @@ export default class CreateRoomPage extends Component {
     });
   }
 
-  handleRoomButtonPressed() {
+  handleRoomButtonPressed() {  
     const requestOptions = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
